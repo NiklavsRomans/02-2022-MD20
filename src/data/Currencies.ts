112544284dff527471b011cd46801e348b583b38
Niklavs Romans <niklavsromans19@gmail.com>
2022-05-19 14:33:55 +0200

    data modal

---
+++ src/data/Currencies.ts
@@ -0,0 +1,45 @@
+export type Currency = {
+    currency: string
+    selected: boolean
+  }
+
+const Currencies: Currency[] = [
+  {
+    currency: 'EUR',
+    selected: false,
+  },
+  {
+    currency: 'PLN',
+    selected: false,
+  },
+  {
+    currency: 'GEL',
+    selected: false,
+  },
+  {
+    currency: 'DKK',
+    selected: false,
+  },
+  {
+    currency: 'CZK',
+    selected: false,
+  },
+  {
+    currency: 'GBP',
+    selected: false,
+  },
+  {
+    currency: 'SEK',
+    selected: false,
+  },
+  {
+    currency: 'USD',
+    selected: false,
+  },
+  {
+    currency: 'RUB',
+    selected: false,
+  },
+];
+
+export default Currencies;
